469d9dfd95b911d06cfc121db41036c497d66b11
Sam Zanca <samuele@zanca.dev>
2023-07-22 14:03:45 -0400

    initia

---
+++ syntax.js
@@ -0,0 +1,48 @@
+// Using js file for the code syntax highlighting
+
+let age = 1;
+let name = "Monkey";
+let result = 10 * (20 / 2);
+
+// -----------------
+
+let myArray = [1, 2, 3, 4, 5];
+
+let thorsten = { "name": "Thorsten", "age": 28 };
+
+// -----------------
+
+myArray[0]       // => 1
+thorsten["name"] // => "Thorsten"
+let add = fn(a, b) { return a + b; };
+
+// -----------------
+
+let add = fn(a, b) { a + b; };
+add(1, 2);
+
+// -----------------
+
+let fibonacci = fn(x) {
+  if (x == 0) {
+    0
+  } else {
+    if (x == 1) {
+      1
+    } else {
+      fibonacci(x - 1) + fibonacci(x - 2);
+    }
+  }
+};
+
+// -----------------
+
+let twice = fn(f, x) {
+  return f(f(x));
+};
+
+let addTwo = fn(x) {
+  return x + 2;
+};
+
+twice(addTwo, 2); // => 6
